dd5a204cf3b03fa7b334b7518f89e43bfac29eec
LavrovNick <lavrovmykola@gmail.com>
2023-04-06 06:34:26 +0300

    refactor verify router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,180 +7,79 @@ const UserVerification = require('../models/UserVerification');
 const { createToken } = require('../utils/handleToken');
 const { sendVerificationEmail } = require('../utils/sendEmail');
 
-router.post('/signup', (req, res) => {
-  let { email, password } = req.body;
-  email = email.trim();
-  password = password.trim();
-  if (email === '' || password === '') {
-    res.json({
-      error: true,
-      message: 'Empty input fields!',
-    });
-  } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-    res.json({
-      error: true,
-      message: 'Invalid email!',
-    });
-  } else if (password.length < 8) {
-    res.json({
-      error: true,
-      message: 'Password is too short!',
-    });
-  } else {
-    User.find({ email }).then((result) => {
+router.post('/signup', async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    email = email.trim();
+    password = password.trim();
+    if (email === '' || password === '') {
+      throw new Error('Empty input fields!');
+    } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      throw new Error('Invalid email!');
+    } else if (password.length < 8) {
+      throw new Error('Password is too short!');
+    } else {
+      const result = await User.find({ email });
       if (result.length) {
-        res.json({
-          error: true,
-          message: 'User already exists!',
-        });
+        throw new Error('User already exists!');
       } else {
-        bcrypt.hash(password, 10).then((hashedPassword) => {
-          const newUser = new User({
-            email,
-            password: hashedPassword,
-            verified: false,
-          });
-          newUser.save().then(({ _id }) => {
-            const uniqueString = uuidv4() + _id;
-            bcrypt
-              .hash(uniqueString, 10)
-              .then((hashedUniqueString) => {
-                const newVerification = new UserVerification({
-                  userId: _id,
-                  uniqueString: hashedUniqueString,
-                  createdAt: Date.now(),
-                  expiresAt: Date.now() + 21600000,
-                });
-
-                newVerification
-                  .save()
-                  .then(async () => {
-                    try {
-                      await sendVerificationEmail({ _id, email, uniqueString });
-                      res.json({
-                        error: false,
-                        message: 'Verification email sent.',
-                      });
-                    } catch (error) {
-                      console.log(error);
-                      res.json({
-                        error,
-                        message: 'An error occured while sending verification code!',
-                      });
-                    }
-                  })
-                  .catch((error) => {
-                    console.log(error);
-                    res.json({
-                      error,
-                      message: 'An error occured while saving verification code!',
-                    });
-                  });
-              })
-              .catch((error) => {
-                res.json({
-                  error,
-                  message: 'An error occured while hashing verification code!',
-                });
-              });
-          }).catch((error) => {
-            console.log(error);
-            res.json({
-              error,
-              message: 'An error occured while saving user account!',
-            });
-          });
-        }).catch((error) => {
-          console.log(error);
-          res.json({
-            error,
-            message: 'An error occured while hashing the password!',
-          });
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+          email,
+          password: hashedPassword,
+          verified: false,
+        });
+        const { _id } = await newUser.save();
+        const uniqueString = uuidv4() + _id;
+        const hashedUniqueString = await bcrypt.hash(uniqueString, 10);
+        const newVerification = new UserVerification({
+          userId: _id,
+          uniqueString: hashedUniqueString,
+          createdAt: Date.now(),
+          expiresAt: Date.now() + 21600000,
+        });
+        await newVerification.save();
+        await sendVerificationEmail({ _id, email, uniqueString });
+        res.json({
+          error: false,
+          message: `Verification email sent to ${email}`,
         });
       }
-    }).catch((error) => {
-      console.log(error);
-      res.json({
-        error,
-        message: 'An error occured while checking for existing user!',
-      });
-    });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error.message);
   }
 });
 
-router.get('/verify/:userId/:uniqueString', (req, res) => {
-  const { userId, uniqueString } = req.params;
-
-  UserVerification
-    .find({ userId })
-    .then((result) => {
-      if (result.length > 0) {
-        const { expiresAt } = result[0];
-        const hashedUniqueString = result[0].uniqueString;
-        if (expiresAt < Date.now()) {
-          UserVerification
-            .deleteOne({ userId })
-            .then(() => {
-              User
-                .deleteOne({ _id: userId })
-                .then(() => {
-                  const message = 'Verification link has expired. Please sign up again';
-                  res.redirect(`/user/verified/error=true&message=${message}`);
-                })
-                .catch((error) => {
-                  console.log(error);
-                  const message = 'An error occured while clearing user with expired verification record.';
-                  res.redirect(`/user/verified/error=true&message=${message}`);
-                });
-            })
-            .catch((error) => {
-              console.log(error);
-              const message = 'An error occured while clearing expired user verification record.';
-              res.redirect(`/user/verified/error=true&message=${message}`);
-            });
-        } else {
-          bcrypt
-            .compare(uniqueString, hashedUniqueString)
-            .then((data) => {
-              if (data) {
-                User.updateOne({ _id: userId }, { verified: true })
-                  .then(() => {
-                    UserVerification.deleteOne({ userId })
-                      .then(() => {
-                        res.sendFile(path.join(__dirname, '../views/verified.html'));
-                      })
-                      .catch((error) => {
-                        console.log(error);
-                        const message = 'An error occured while deleting existing verification record.';
-                        res.redirect(`/user/verified/error=true&message=${message}`);
-                      });
-                  })
-                  .catch((error) => {
-                    console.log(error);
-                    const message = 'An error occured while updating user record to show verified.';
-                    res.redirect(`/user/verified/error=true&message=${message}`);
-                  });
-              } else {
-                const message = 'Invalid verification details. Check your email inbox again.';
-                res.redirect(`/user/verified/error=true&message=${message}`);
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-              const message = 'An error occured while comparing unique strings for verification record.';
-              res.redirect(`/user/verified/error=true&message=${message}`);
-            });
-        }
+router.get('/verify/:userId/:uniqueString', async (req, res) => {
+  try {
+    const { userId, uniqueString } = req.params;
+    const result = await UserVerification.find({ userId });
+    if (result.length > 0) {
+      const { expiresAt } = result[0];
+      const hashedUniqueString = result[0].uniqueString;
+      if (expiresAt < Date.now()) {
+        await UserVerification.deleteOne({ userId });
+        await User.deleteOne({ _id: userId });
+        throw new Error('Verification link has expired. Please sign up again');
       } else {
-        const message = "Account verification record doesn't exist or has been verified already. Please sign up or log in.";
-        res.redirect(`/user/verified/error=true&message=${message}`);
+        const data = await bcrypt.compare(uniqueString, hashedUniqueString);
+        if (data) {
+          await User.updateOne({ _id: userId }, { verified: true });
+          await UserVerification.deleteOne({ userId });
+          res.sendFile(path.join(__dirname, '../views/verified.html'));
+        } else {
+          throw new Error('Invalid verification details. Check your email inbox again.');
+        }
       }
-    })
-    .catch((error) => {
-      console.log(error);
-      const message = 'An error occured while checking for existing verification record.';
-      res.redirect(`/user/verified/error=true&message=${message}`);
-    });
+    } else {
+      throw new Error("Account verification record doesn't exist or has been verified already. Please sign up or log in.");
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect(`/user/verified?error=true&message=${error.message}`);
+  }
 });
 
 router.get('/verified', (req, res) => {
